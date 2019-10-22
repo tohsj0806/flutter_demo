d36cd687a46e147bff4b7624f33a193af21ef53c
“tohsj0806@qq.com <tohsj0806@qq.com>
2019-10-22 11:17:35 +0800

    上拉刷新

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -52,7 +52,7 @@ class _HomePageState extends State<HomePage> {
             },
           ),
           RaisedButton(
-            child: Text('Per'),
+            child: Text('下拉刷新上拉加载'),
             onPressed: () {
                 //todo
             },
